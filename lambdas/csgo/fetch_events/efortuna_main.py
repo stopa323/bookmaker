bf3ebb785cc2e270478b48f13820dcd4721498a9
stopa323 <stopa323@gmail.com>
2020-07-26 11:26:05 -0400

    Push collected URLs to SQS queue

--- lambdas/csgo/fetch_events/efortuna_main.py
+++ lambdas/csgo/fetch_events/efortuna_main.py
@@ -1,10 +1,16 @@
 import aiohttp
 import asyncio
+import boto3
 
 from bs4 import BeautifulSoup
+from os import environ
 from typing import List
 
 URL = "https://www.efortuna.pl/zaklady-bukmacherskie/esport-cs-go"
+
+# Create SQS cliend
+sqs = boto3.client('sqs')
+SQS_QUEUE_URL = environ["SQS_QUEUE_URL"]
 
 
 async def fetch_event_url_list() -> List[str]:
@@ -31,5 +37,13 @@ def extract_event_links(page_html: str) -> List[str]:
 def handler(event, ctx):
     loop = asyncio.get_event_loop()
     event_links = loop.run_until_complete(fetch_event_url_list())
+
+    # Send message to SQS queue for each obtained URL
+    for link in event_links:
+        # TODO: Find out if this may fail somehow
+        sqs.send_message(
+            QueueUrl=SQS_QUEUE_URL,
+            MessageBody=link
+        )
 
     return {"statusCode": 200}
